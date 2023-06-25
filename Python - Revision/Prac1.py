589be98be242a9a2ce95328e5c28f96f70bc567e
11akanksha <64818557+11akanksha@users.noreply.github.com>
2023-06-26 00:51:16 +0530

    Create Prac1.py

---
+++ Python - Revision/Prac1.py
@@ -0,0 +1,13 @@
+# Ques: https://www.practicepython.org/exercise/2014/01/29/01-character-input.html
+
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+
+print("Hi ", name, ", you'll turn 100 in ", 100 - age, " years")
+
+#O/p:
+
+Enter your name: Ava
+Enter your age: 23
+Hi  Ava , you'll turn 100 in  77  years
+
