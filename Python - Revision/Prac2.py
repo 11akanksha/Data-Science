e9975ad634ce208cb91a737848dd9f652a02702b
11akanksha <64818557+11akanksha@users.noreply.github.com>
2023-06-26 00:55:43 +0530

    Create Prac2.py

---
+++ Python - Revision/Prac2.py
@@ -0,0 +1,7 @@
+# Ques: https://www.practicepython.org/exercise/2014/02/05/02-odd-or-even.html
+
+no = int(input("Enter a number: "))
+if no % 2 == 0:
+    print("The number ", no, " is an even number")
+else:
+    print("The number is odd")
